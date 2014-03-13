c429e9e2586649d2fbcd5eb2376ca0c43c4c3238
Allen Wu <allen.wu@originate.com>
2014-03-12 19:30:00 -0700

    Initial Commit

---
+++ OpenGLDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLDemo
+//
+//  Created by Allen Wu on 3/12/14.
+//  Copyright (c) 2014 Originate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenGLDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenGLDemo
+//
+//  Created by Allen Wu on 3/12/14.
+//  Copyright (c) 2014 Originate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
